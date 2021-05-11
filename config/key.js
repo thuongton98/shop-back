c898bacabb248d2af72e5e6b918942fdd46ebf27
thuongton98 <thuonglovevietnam@gmail.com>
2021-05-11 21:08:09 +0700

    T

--- config/key.js
+++ config/key.js
@@ -1,6 +1,6 @@
-dbPassword='mongodb://localhost:27017/shop'
+//dbPassword='mongodb://localhost:27017/shop'
 
-//dbPassword = 'mongodb+srv://thuongadmin:thuong123@cluster0.gt3dp.mongodb.net/MyShop?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://thuongadmin:thuong123@cluster0.gt3dp.mongodb.net/MyShop?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
